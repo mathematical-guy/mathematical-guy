4d53c44e0d153476e40cd33419948c72d2f2b11f
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2023-12-30 21:16:30 +0530

    Remove print statements

--- mathematical_guy/settings.py
+++ mathematical_guy/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
-print(BASE_DIR / ".env")
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
