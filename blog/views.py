03c5c9f1b0ba250483d41c2b9af5bba918f140b5
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2023-12-30 19:08:21 +0530

    Added Image display handling

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from blog.models import BlogComponent
+from blog.models import BlogComponent, Blog
 
 
 class BlogComponentListView(generic.ListView):
@@ -11,3 +11,9 @@ class BlogComponentListView(generic.ListView):
     def get_queryset(self):
         blog_id = self.kwargs.get('blog_id')
         return BlogComponent.objects.filter(blog__id=blog_id)
+
+    def get_context_data(self, **kwargs):
+        context: dict = super(BlogComponentListView, self).get_context_data(**kwargs)
+        blog = Blog.objects.filter(id=self.kwargs.get('blog_id')).first()
+        context["blog_title"] = blog.title if blog else "N/A"
+        return context
